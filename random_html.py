6f42f56772b043623fa6f0a3ae137f563ca9be75
MidLeona <aionios0821@gmail.com>
2024-06-23 15:21:06 +0900

    Add random_html.py and welcome_page.py

---
+++ random_html.py
@@ -0,0 +1,9 @@
+from flask import Flask
+import random
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return 'random : <strong>' + str(random.random()) + '</strong>'
+
+app.run(debug = True)
---
+++ welcome_page.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import random
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+  return 'Welcome'
+
+@app.route('/create/')
+def create():
+  return 'Create new page'
+
+@app.route('/read/<id>/')
+def read(id):
+  return 'Read ' + id + ' page'
+
+app.run(debug = True)
