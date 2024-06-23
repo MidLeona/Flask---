9a2fa68419622b7b70a98c64b83287d14adfc34e
MidLeona <aionios0821@gmail.com>
2024-06-23 15:27:23 +0900

    플라스크 실습

--- random_html.py
+++ random_html.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def index():
   return 'random : <strong>' + str(random.random()) + '</strong>'
 
-app.run(debug = True)
+app.run(debug = True)  
--- welcome_page.py
+++ welcome_page.py
@@ -14,4 +14,4 @@ def create():
 def read(id):
   return 'Read ' + id + ' page'
 
-app.run(debug = True)
+app.run(debug = True)  
